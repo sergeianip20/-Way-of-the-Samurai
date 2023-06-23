82567c98b100906ff25caf47c082db440c9f474d
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-06-23 10:41:56 +0400

    Update menu.stories.ts

--- src/stories/menu.stories.ts
+++ src/stories/menu.stories.ts
@@ -1,8 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import {Menu} from '../component/Menu/Menu'
 const meta: Meta<typeof Header> = {
-  title: 'Example/Header',
-  component: Header,
+  title: 'Example/Menu',
+  component: Menu,
 } 
 export default meta;
 type Story = StoryObj<typeof Header>;
