21a660e85bff9710f0d7368ffdde20f5068df98c
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-06-23 11:03:11 +0400

    Create login.stories.ts

---
+++ src/stories/login.stories.ts
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import {Login} from '../component/Login/Login'
+const meta: Meta<typeof Login> = {
+  title: 'Example/Login',
+  component: Login,
+} 
+export default meta;
+type Story = StoryObj<typeof Login>;
+
+
+
+export const LoginIn: Story = {
+  args: {}
+}
