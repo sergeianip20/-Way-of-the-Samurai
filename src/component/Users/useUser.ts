e2895a269f69ca900a72ff87da3cbe9ba7207970
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-06-23 18:43:12 +0400

    Update useUser.ts

--- src/component/Users/useUser.ts
+++ src/component/Users/useUser.ts
@@ -1,13 +1,24 @@
 import React , {useCallback} from 'react'
-
+import {useAppDispatch} from "component/hook/hook";
 
 
 export const useUser =()=>{
   const dispatch = useAppDispatch()
 const onChangeHandler = useCallback((e:any)=> {
         dispatch(userThunk.usersFetch({currentPage:e, pageSize:5}))
-    },[])
+    },[dispatch])
+  const onClickFolowed =useCallback((userId:string)=> {
+     dispatch(userThunk.followedThunk(userId) )
+   },[dispatch])
+const onClickUnFollowed=(userId:string)=>{
+    dispatch(userThunk.unFollowed(userId))
+  }
 
-
-  return {}
+  return {
+onChangeHandler,
+    onClickFolowed,
+    
+onClickUnFollowed
+    
+  }
 }
