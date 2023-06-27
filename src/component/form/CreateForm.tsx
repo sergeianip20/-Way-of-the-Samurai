dbd405a9f3a030f913bfb7cb4725808e62fd9a9a
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-06-27 12:44:22 +0400

    Update CreateForm.tsx

--- src/component/form/CreateForm.tsx
+++ src/component/form/CreateForm.tsx
@@ -1,10 +1,29 @@
+import {SubmitHandler, useForm} from "react-hook-form";
+import {useAppSelector} from 
+
 export const ProfileForm = () => {
     const {register, watch, handleSubmit} = useForm();
-
+    const userId = useAppSelector((state)=> state.authReducer.userId)
+    const onSubmit:SubmitHandler =data => {
+       let apiModel ={
+      contacts: {
+            facebook: data.facebook,
+            github: data.github,
+            instagram: data.instagram,
+            mainLink: data.mainLink,
+            twitter: data.twitter,
+            vk: data.vk,
+            website: data.website,
+            youtube: data.youtube,
+               }
+     userId
+         fullName: data.fullName,  
+       }
+    }
     return (<>
         <Row>
             <Card title='Изменения профиля' bordered={false} style={{width: 1200}}>
-                <Form>
+                <Form handleSubmit={onSubmit}>
                 <Row>
                     <Col span={4}>Имя</Col>
                     <Col span={20}>
