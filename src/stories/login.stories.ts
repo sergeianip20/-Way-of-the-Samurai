0d2d6cbc8073ec37770d6ae25cc9599be9d7d9dc
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-06-27 19:14:16 +0400

    Update login.stories.ts

--- src/stories/login.stories.ts
+++ src/stories/login.stories.ts
@@ -1,8 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import {Login} from '../component/Login/Login'
+import {ReduxStoreProviderDecorator} from './ReduxStoreProviderDecorator'
 const meta: Meta<typeof Login> = {
   title: 'Example/Login',
   component: Login,
+  decorator: ReduxStoreProviderDecorator
 } 
 export default meta;
 type Story = StoryObj<typeof Login>;
