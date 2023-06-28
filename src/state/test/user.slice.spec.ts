f95d0b6ecad8ae27e7045302b368e22e46ed7ddd
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-06-28 14:15:41 +0400

    Update user.slice.spec.ts

--- src/state/test/user.slice.spec.ts
+++ src/state/test/user.slice.spec.ts
@@ -1,1 +1,36 @@
 import {userThunk , Userreducer  } from '../state/UserReducer'
+descride('authReducer', ()=>{
+export type InitialStateUserType = {
+    users: Array<usersTypeData>
+    pagesSize: number
+    totalUsersCount: number,
+    currentPage: number
+    preloader: boolean,
+    toggleisFollowed: Array<number>
+}
+export type usersTypeData = {
+    id: number,
+    followed: boolean
+    name: string
+    status: string | null
+    photos: {
+        large: string | null
+        small: string | null
+    }
+    uniqueUrlName: string | null
+}
+let initialState: InitialStateUserType = {
+    users: [],
+    pagesSize: 5,
+    totalUsersCount: 20,
+    currentPage: 1,
+    preloader: true,
+    toggleisFollowed: []
+}
+it('fetchUser', ()=>{
+
+
+  
+})
+    
+})
