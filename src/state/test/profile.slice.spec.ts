b97d3501f3a9fab1776dc3c02d342ecaff93bec5
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-06-28 11:06:03 +0400

    Update profile.slice.spec.ts

--- src/state/test/profile.slice.spec.ts
+++ src/state/test/profile.slice.spec.ts
@@ -1,1 +1,35 @@
+descride('authReducer', ()=>{
+let initialState = {
 
+    PostData: [
+        {id: 1, post: 'mypost number 1', likesCoint: 1},
+        {id: 2, post: 'mypost number 2', likesCoint: 3},
+        {id: 3, post: 'mypost number 3', likesCoint: 12},
+    ],
+    newPostText: '',
+    UserProfile: {
+        aboutMe: ' ',
+        contacts: {
+            facebook: ' ',
+            github: '',
+            instagram: '',
+            mainLink: '',
+            twitter: '',
+            vk: '',
+            website: '',
+            youtube: ' ',
+        },
+        fullName: ' ',
+        lookingForAJob: true,
+        lookingForAJobDescription: ' ',
+        userId: 10,
+        photos: {
+            large: ' ',
+            small: ' '
+        }
+    }
+}
+
+
+  
+})
